52119902a31c396fea45c8a5119ab1bea9d65864
Simbongile Mkhotheli <mkotelisimbo@gmail.com>
2025-06-01 16:54:26 +0200

    setup: add redux-persist and react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './Styles/index.css';
-import { Provider } from 'react-redux';
-import Store from './app/Store.js';
-import { Toaster } from 'react-hot-toast';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './Styles/index.css'
+import { Provider } from 'react-redux'
+import Store, { persistor } from './app/Store' // include persistor
+import { PersistGate } from 'redux-persist/integration/react'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Store}>
-      <Toaster position="top-center" reverseOrder={false} />
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <Toaster position="top-center" reverseOrder={false} />
+        <App />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
