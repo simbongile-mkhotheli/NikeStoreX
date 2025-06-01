aa4ead2259b0bb4ac4bdd1d9a04e04d17831f4d7
Simbongile Mkhotheli <mkotelisimbo@gmail.com>
2025-06-01 16:51:48 +0200

    setup: configure redux-persist with cart slice

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,11 +1,21 @@
 // src/app/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './features/cart/cartSlice.js'; // fixed casing to match actual file
-
+import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import cartReducer from './features/cart/cartSlice.js'
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, cartReducer)
 const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistedReducer,
   },
-});
-
-export default store;
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+})
+export const persistor = persistStore(store)
+export default store
